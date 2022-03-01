f9cc042b5863f612260f98d92dbf960b20a04583
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2022-03-01 19:22:54 -0300

    add implementation to service

--- CalculaJuros.Services/CalculaJurosService.cs
+++ CalculaJuros.Services/CalculaJurosService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CalculaJuros.Models;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace CalculaJuros.Services
 {
@@ -13,9 +17,19 @@ namespace CalculaJuros.Services
             _httpRequester = httpRequester;
         }
 
-        public Task<decimal> CalculaJuro(decimal valorInicial, int tempoMeses)
+        public async Task<decimal> CalculaJuro(decimal valorInicial, int tempoMeses)
         {
-            throw new NotImplementedException();
+            var response = await _httpRequester.GetLastJuroAsync();
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new Exception("Request error.");
+            
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var juroDto = JToken.Parse(responseContent)
+                .ToObject<JuroDto>();
+
+            return juroDto.Taxa;
         }
     }
 }
